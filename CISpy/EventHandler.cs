427c06e337715adad53ed2e2f616be6d9164b980
Todd <infernogaming621@gmail.com>
2019-01-15 15:08:32 -0500

    made colors less eye piercing

--- CISpy/EventHandler.cs
+++ CISpy/EventHandler.cs
@@ -104,7 +104,7 @@ namespace CISpy
 					if (!Plugin.SpyDict[ev.Attacker.SteamId])
 					{
 						Plugin.SpyDict[ev.Attacker.SteamId] = true;
-						ev.Attacker.PersonalBroadcast(10, $"<color=#d0d0d0>You have attacked a {(ev.Player.TeamRole.Team == Smod2.API.Team.NINETAILFOX ? "<color=\"blue\">Nine Tailed Fox" : "<color=\"yellow\">Scientist")}</color>, you are now able to be <color=\"red\">killed</color> by <color=\"blue\">Nine Tailed Fox</color> and <color=\"yellow\">Scientists</color>.</color>", false);
+						ev.Attacker.PersonalBroadcast(10, $"<color=#d0d0d0>You have attacked a {(ev.Player.TeamRole.Team == Smod2.API.Team.NINETAILFOX ? "<color=#00b0fc>Nine Tailed Fox" : "<color=#fcff8d>Scientist")}</color>, you are now able to be killed by <color=#00b0fc>Nine Tailed Fox</color> and <color=#fcff8d>Scientists</color>.</color>", false);
 						ev.Player.SetHealth(ev.Player.GetHealth() - (int)ev.Damage);
 					}
 					else
